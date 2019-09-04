a55c1a568de9ce387dcdb3303e59e2ed389624d2
RuneThe <runethelen@uib.no>
2019-09-04 13:41:54 +0200

    changes in calory-functions

--- functions/caloriesMen.js
+++ functions/caloriesMen.js
@@ -1,4 +1,4 @@
-export default function caloriesMen(age, weight, height, activityIndex) {
+export default function caloriesMen(birthYear, weight, height, activityIndex) {
   // BMR = 66.5 + ( 13.75 x vekt i kg ) + ( 5.003 x høyde i cm ) – ( 6.755 x alder i år )
-  return (65.5 + (13.75 * weight)+(5.003 * height)-(6.755 * age)) * activityIndex;
+  return (65.5 + (13.75 * weight)+(5.003 * height)-(6.755 * (Date.getYear()-birthYear))) * activityIndex;
 }
