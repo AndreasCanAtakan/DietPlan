6549a91dc958aedbd7ab66487fb8db47a6845db1
RuneThe <runethelen@uib.no>
2019-09-19 10:07:25 +0200

    updates

--- TestRUTINER/iCALoutput.js
+++
@@ -1,9 +0,0 @@
-msgData1 = $('.start-time').text();
-    msgData2 = $('.end-time').text();
-    msgData3 = $('.Location').text();
-
-    var icsMSG = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:me@google.com\nDTSTAMP:20120315T170000Z\nATTENDEE;CN=My Self ;RSVP=TRUE:MAILTO:me@gmail.com\nORGANIZER;CN=Me:MAILTO::me@gmail.com\nDTSTART:" + msgData1 +"\nDTEND:" + msgData2 +"\nLOCATION:" + msgData3 + "\nSUMMARY:Our Meeting Office\nEND:VEVENT\nEND:VCALENDAR";
-
-    $('.test').click(function(){
-        window.open( "data:text/calendar;charset=utf8," + escape(icsMSG));
-    });
